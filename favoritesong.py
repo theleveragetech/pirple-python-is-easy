e94fe7e0b114ab7d1e00ee0d91806a76a0405d1c
theleveragetech <35285301+theleveragetech@users.noreply.github.com>
2020-01-26 03:25:05 -0500

    Favorite Song
    This Python code explains what one of my favorite songs is, and prints some facts about the song.

---
+++ favoritesong.py
@@ -0,0 +1,24 @@
+'''
+This Python program contains information on one of my favorite songs,
+and prints some facts about the song.
+''
+
+#Song Information and Facts, declared as Variables
+Genre ="R & B"
+Artist = "Mary J. Blige"
+Song = "My Life"
+AlbumTitle = "My Life"
+DurationInSeconds = 257
+YearOfRelease = 1994
+MusicRecordCompany = "Geffen Records"
+ExecutiveProducers = "Sean (Diddy) Combs, Andre Harrell"
+
+#Print all song information and facts
+print(Genre)
+print(Artist)
+print(Song)
+print(AlbumTitle)
+print(DurationInSeconds)
+print(YearOfRelease)
+print(MusicRecordCompany)
+print(ExecutiveProducers)
